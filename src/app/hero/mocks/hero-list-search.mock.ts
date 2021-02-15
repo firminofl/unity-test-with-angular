9547ba839606356dcd707eb910040ec69197f3ec
Filipe Firmino <filipe.firmino@leucotron.com.br>
2021-02-15 20:46:12 -0300

    :construction: add hero-list-search-name mock
    Signed-off-by: Filipe Firmino <filipe.firmino@leucotron.com.br>

---
+++ src/app/hero/mocks/hero-list-search.mock.ts
@@ -0,0 +1,22 @@
+import { IHeroListSearchName } from "../dtos/hero-list-search-name.dto";
+
+export const mockHeroListSearchName: IHeroListSearchName[] = [
+    {
+        userId: 1,
+        id: 1,
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body: "nostrum rerum est autem sunt rem eveniet architecto"
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "qui est esse",
+        body: "aperiam non debitis possimus qui neque nisi nulla"
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        body: "molestiae porro eius odio et labore et velit aut"
+      },
+]
